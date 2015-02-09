ad49320dd3281ecdedc7293b0bece262e7160981
tattii <yuta.tatti@gmail.com>
2015-02-10 04:15:35 +0900

    add emmet

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.emmet ~/.emmet
